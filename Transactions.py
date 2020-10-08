1a2cfe203551bacd6a500cd619e42d8af19ebab6
FrictionalUnemployment <62852896+FrictionalUnemployment@users.noreply.github.com>
2020-10-08 20:48:07 +0200

    Update Transactions.py

--- Transactions.py
+++ Transactions.py
@@ -4,11 +4,12 @@ from datetime import datetime
 
 class Transactions:
     
-    def __init__(self, tid, tfee):
-        self.t = tid # The Transaction ID is stored here
-        self.f = tfee # The Priority (based on amount of fee is stored here) 
-        self.s = getTimestamp() #Time stamp is stored here
-
+    def __init__(self, tid, tfee, tsize):
+        self.id = tid # The Transaction ID is stored here
+        self.fee = tfee # The Priority (based on amount of fee is stored here) 
+        self.stamp = getTimestamp() #Time stamp is stored here
+        self.size = tsize #Transaction size is stored here
+        
 #Get the current local timestamp
 def getTimestamp():
     now = datetime.now()
@@ -16,5 +17,5 @@ def getTimestamp():
 
 #Example code to utilize this class is to use:
 #my_arr = []
-#my_arr.append(Transactions(1, 30))
-#my_arr.append(Transactions(2, 40))
+#my_arr.append(Transactions(1, 30, 300))
+#my_arr.append(Transactions(2, 40, 400))
